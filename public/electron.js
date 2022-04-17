4ed58840daa6f9452d40f75593dc7793c204a931
fstylermiller <tylermiller@gmail.com>
2022-04-16 23:12:00 -0700

    Added IPC, Dialogs

---
+++ public/electron.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  dialog
+} = require('electron');
+// require('@electron/remote/main').initialize();
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: `${__dirname}/preload.js`
+    },
+  });
+
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+  // Open the DevTools.
+  if (isDev) {
+    win.webContents.openDevTools({ mode: 'detach' });
+  }
+}
+
+app.whenReady().then(() => {
+  ipcMain.handle('dialog', (_event, method, params) =>
+    dialog[method](params)
+  );
+  createWindow();
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    const dialogConfig = {
+      title: 'Select images, or a folder',
+      properties: [
+        'openFile',
+        'openDirectory',
+        'multiSelections'
+      ],
+    };
+    electron.openDialog('showOpenDialog', dialogConfig)
+      .then(result => console.log("RESULT: ", result));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>
+        Open Dialog
+      </button>
     </div>
   );
 }
